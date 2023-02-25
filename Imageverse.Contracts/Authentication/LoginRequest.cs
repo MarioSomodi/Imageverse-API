88dbe676cbe97252c7259f8305f12d1d9d74e08b
MarioSomodi <mario0.0somodi@gmail.com>
2023-02-25 18:53:02 +0100

    Changed classes to they are initialized as records

--- Imageverse.Contracts/Authentication/LoginRequest.cs
+++ Imageverse.Contracts/Authentication/LoginRequest.cs
@@ -1,8 +1,8 @@
 ﻿namespace Imageverse.Contracts.Authentication
 {
     public record LoginRequest
-    {
-        public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
-    }
+    (
+        string Email,
+        string Password
+    );
 }
--- Imageverse.Contracts/Authentication/RegisterRequest.cs
+++ Imageverse.Contracts/Authentication/RegisterRequest.cs
@@ -1,13 +1,13 @@
 ﻿namespace Imageverse.Contracts.Authentication
 {
     public record RegisterRequest
-    {
-        public int PackageId { get; set; }
-        public string Username { get; set; } = null!;
-        public string Name { get; set; } = null!;
-        public string Surname { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public string ProfileImage { get; set; } = null!;
-        public string Password { get; set; } = null!;
-    }
+    (
+        int PackageId,
+        string Name,
+        string Username,
+        string Surname,
+        string Email,
+        string ProfileImage,
+        string Password
+    );
 }
