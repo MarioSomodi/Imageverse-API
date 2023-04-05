4117313ba04501f8dfeaa668e45a38589cabe09b
MarioSomodi <mario0.0somodi@gmail.com>
2023-04-05 19:47:26 +0200

    Implemented base classes -> All the logic for comparing two entiter or value objects completed -> Value objects are the same when all their values are the same -> Entites are the same when their Id is the same

---
+++ Imageverse.Domain/Models/AggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿namespace Imageverse.Domain.Models
+{
+    public abstract class AggregateRoot<TId> : Entity<TId>
+        where TId : notnull
+    {
+        protected AggregateRoot(TId id) : base(id)
+        {
+        }
+    }
+}
---
+++ Imageverse.Domain/Models/ValueObject.cs
@@ -0,0 +1,43 @@
+﻿namespace Imageverse.Domain.Models
+{
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {
+        public abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+            
+            var valueObject = (ValueObject)obj;
+
+            return GetEqualityComponents()
+                .SequenceEqual(valueObject.GetEqualityComponents());
+        }
+
+        public static bool operator ==(ValueObject left, ValueObject right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ValueObject left, ValueObject right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override int GetHashCode()
+        {
+            //^ -> XOR operator if the values are the same it will return 0 else 1
+            return GetEqualityComponents()
+                .Select(x => x?.GetHashCode() ?? 0)
+                .Aggregate((x, y) => x ^ y);
+        }
+
+        public bool Equals(ValueObject? other)
+        {
+            return Equals ((object?)other);
+        }
+    }
+}
